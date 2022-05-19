8412cf580235a49acf2def88f6e82ddd66dd456f
MarioMolinaRoldan <mariomolrol@campus.monlau.com>
2022-05-19 15:49:36 +0200

    scss a css

--- angular-ecommerce-app-master/client/src/app/auth/components/register/register.component.ts
+++ angular-ecommerce-app-master/client/src/app/auth/components/register/register.component.ts
@@ -6,7 +6,7 @@ import { ApiService } from '../../../services/api.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   fullName = '';

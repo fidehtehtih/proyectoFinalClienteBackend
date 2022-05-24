b7feb41d4eb77112c015a5ace61844d726c89d07
edgarrosarey <edgarrosarey@gmail.com>
2022-05-24 18:52:51 +0200

    ok

--- CharcuteriaLosTocinos/client/src/app/panel-admin/panel-admin.component.ts
+++ CharcuteriaLosTocinos/client/src/app/panel-admin/panel-admin.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { ProductService } from '../services/product.service';
 import { Product } from '../shared/models/product.model';
+import { ApiService } from '../services/api.service'; 
+
 @Component({
   selector: 'app-panel-admin',
   templateUrl: './panel-admin.component.html',
@@ -9,35 +11,19 @@ import { Product } from '../shared/models/product.model';
 })
 export class PanelAdminComponent implements OnInit {
 
-  product:Product;
-
-  MyNewForm = new FormGroup({
-    id: new FormControl(''),
-    nombre: new FormControl(''),
-    imagen: new FormControl(''),
-    description: new FormControl(''),
-    precio: new FormControl(''),
-    cantidad: new FormControl(''),
-    categoria: new FormControl(''),
-    categoriaId: new FormControl(''),
-    
-  });
-
-  constructor() { }
+  fullName = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  errorMessage = '';
+  constructor(
+    private _api: ApiService,
+  ) {}
 
   ngOnInit(): void {
   }
-  Click(datos) { 
-    console.log(datos); 
+  onSubmit() { 
+    this.errorMessage = '';
 
-
-    this.product.id = datos.id;
-    this.product.name = datos.nombre;
-    this.product.image = datos.imagen;
-    this.product.description = datos.description;
-    this.product.price = datos.precio;
-    this.product.quantity = datos.cantidad;
-    this.product.images = datos.categoria;
-    this.product.category = datos.categoriaId;
-  }
+}
 }

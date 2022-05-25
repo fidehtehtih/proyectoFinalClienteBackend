c5a9b862ef9fddccbe54d59508f4b26f2af4322f
MarioMolinaRoldan <mariomolrol@campus.monlau.com>
2022-05-25 17:49:23 +0200

    ok

--- CharcuteriaLosTocinos/client/src/app/home-principal/home-principal.component.ts
+++ CharcuteriaLosTocinos/client/src/app/home-principal/home-principal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home-principal',
   templateUrl: './home-principal.component.html',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePrincipalComponent implements OnInit {
 
+
+
   constructor() { }
 
+
+  public slides: string[] = ['./assets/img10.jpg', './assets/img18.jpg', './assets/img12.jpg', './assets/img13.jpg']
+  i: number;
+
+  showSlide(slides, i) {
+    let slide = slides[i];
+    return slide;
+  }
+
+  getPrev(slides, i) {
+    this.i = this.i - 1;
+    this.showSlide(slides, i)
+  }
+
+  getNext(slides, i) {
+    this.i = this.i + 1;
+    this.showSlide(slides, i)
+  }
+
   ngOnInit(): void {
+    this.i = 1;
   }
 
 }

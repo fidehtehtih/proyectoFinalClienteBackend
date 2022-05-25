cecae5b0483b00ebf960b3f3e20ebbda5643184e
fidehtehtih <alexplanellasgarcia@gmail.com>
2022-05-25 17:20:53 +0200

    ok

--- CharcuteriaLosTocinos/client/src/app/shared/models/product.model.ts
+++ CharcuteriaLosTocinos/client/src/app/shared/models/product.model.ts
@@ -6,10 +6,11 @@ export interface Products {
 export interface Product {
   id: Number;
   name: String;
-  category: String;
-  description: String;
   image: String;
-  price: Number;
-  quantity: Number;
   images: String;
+  price: Number;
+  description: String;
+  category: String;
+  quantity: Number;
+  
 }

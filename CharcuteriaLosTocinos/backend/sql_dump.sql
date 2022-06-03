361405de0aed778e7c64d0c0046849a41676234c
walter03gabriel <walter.gabreil.03@gmail.com>
2022-06-03 15:53:01 +0200

    ok

--- CharcuteriaLosTocinos/backend/sql_dump.sql
+++ CharcuteriaLosTocinos/backend/sql_dump.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 2, 10020 at 09:26 PM
--- Server version: 8.0.17
--- PHP Version: 7.3.8
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
